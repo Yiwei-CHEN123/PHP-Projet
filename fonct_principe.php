b0ca25e4ee3a3bfc6069a0732a3e71c049bed007
Yiwei-CHEN123 <64252600+Yiwei-CHEN123@users.noreply.github.com>
2020-04-29 23:43:37 +0200

    Add files via upload

---
+++ fonct_principe.php
@@ -0,0 +1,41 @@
+<?php
+   // charger le fichier des fonctions
+require("fonction_utile.php");
+
+// executer la fonction de connexion
+$cx = connexion(); 
+session_start();
+
+//$email = filter_input(INPUT_GET, "email", FILTER_SANITIZE_SPECIAL_CHARS);
+
+?>
+
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <title>La fonction principale</title>
+</head>
+
+<body>
+
+    <p>Choisissez l'opération que vous voulez faire :</p>
+
+    <br/>
+    <?php
+        // Faire la distinction pour les différents utilisateurs
+        $PosteE = retrouverPoste($cx,$_SESSION["email"]);
+        if ($PosteE == "Directeur" or $PosteE == "Directeur Adjoint" or $PosteE == "Directeur des ventes") {
+            echo '<a href="commande.php">Faire une commande de rapport</a>';
+            echo ("<br/>");
+            echo '<a href="liste_rapport.php">Consulter les rapports</a>';
+        } else {
+            echo '<a href="liste_rapport.php">Consulter les rapports</a>';
+        } 
+        // Faire la session pour conserver l'information du poste d'utilisateur
+        $_SESSION['Poste'] = $PosteE;
+    ?>
+
+</body>
+
+</html>
