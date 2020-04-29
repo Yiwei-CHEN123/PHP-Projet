b89f3e4ef616dea2bb1fa2a28fcd9b2617c58cc2
julien31-c <64418421+julien31-c@users.noreply.github.com>
2020-04-29 15:40:58 +0200

    Update ajout_new_indic.php

--- tableau_de_bord/ajout_new_indic.php
+++ tableau_de_bord/ajout_new_indic.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
@@ -13,6 +12,7 @@ and open the template in the editor.
 }
 ?>
 
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
