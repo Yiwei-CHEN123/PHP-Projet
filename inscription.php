70eb9a5764e4ee5da8e71157276b11a45b46f5d1
Shanshan-Zhao <64408538+Shanshan-Zhao@users.noreply.github.com>
2020-04-30 19:08:31 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -13,7 +13,7 @@
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 
-	<body id="body0">
+	<body id="bodyIndex">
 		<h1>Lagard&egrave;re Active</h1>
 		<p>Veuillez fournir les informations suivantes:</p>
 		
