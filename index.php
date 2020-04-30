a55b058c391798097e1c5807339a97af5ecd42f7
Shanshan-Zhao <64408538+Shanshan-Zhao@users.noreply.github.com>
2020-04-30 19:37:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	<head>
 		<title>Lagard&egrave;re Active</title>
 		<meta charset="utf-8" />
-		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="style1.css">
 	</head>
 
 	<body id="bodyIndex">
